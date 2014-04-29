185c426ebec660f0fc338c5f0ca6c0265dbe9f47
Daniel Neis Araujo <danielneis@gmail.com>
2014-04-29 10:15:52 -0300

    ignoring automatically generated stuff

--- gen/R.java.d
+++
@@ -1,8 +0,0 @@
-/arquivos2/work/sushi-tcc/gen/com/sushi/saude/R.java \
- : /arquivos2/work/sushi-tcc/res/layout/main.xml \
-/arquivos2/work/sushi-tcc/res/drawable-hdpi/ic_launcher.png \
-/arquivos2/work/sushi-tcc/res/drawable-mdpi/ic_launcher.png \
-/arquivos2/work/sushi-tcc/res/values/strings.xml \
-/arquivos2/work/sushi-tcc/res/drawable-ldpi/ic_launcher.png \
-/arquivos2/work/sushi-tcc/res/drawable-xhdpi/ic_launcher.png \
-/arquivos2/work/sushi-tcc/bin/AndroidManifest.xml \
